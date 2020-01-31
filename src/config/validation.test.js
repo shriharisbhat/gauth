6892821f2e27515979e248bd00141dfdfdfe2015
Raghavendra Mogeraya <raghavendra.mogeraya@robosoftin.com>
2020-01-31 19:01:57 +0530

    added unit cases for validation.js

--- src/config/validation.test.js
+++ src/config/validation.test.js
@@ -3,6 +3,13 @@ import { shallow, mount } from "enzyme";
 import { checkValidity } from "./validation";
 
 describe(" validation should return values", () => {
+  it(" - send a valid email,required as false and it should return false", () => {
+    let result = checkValidity("admin@gmail.com", {
+      required: false,
+      email: true
+    });
+    expect(result).toBeTruthy();
+  });
   it(" - send a invalid email and it should return false", () => {
     let result = checkValidity("admi@gmail", {
       required: true,
@@ -45,6 +52,38 @@ describe(" validation should return values", () => {
     let result = checkValidity("Admin123!", {
       required: true,
       password: true
+    });
+    expect(result).toBeTruthy();
+  });
+
+  it(" - send a string with less than minLength and it should return false", () => {
+    let result = checkValidity("Dis", {
+      required: true,
+      minLength: 4
+    });
+    expect(result).toBeFalsy();
+  });
+
+  it(" - send a string with more than minLength and it should return true", () => {
+    let result = checkValidity("Discovery", {
+      required: true,
+      minLength: 4
+    });
+    expect(result).toBeTruthy();
+  });
+
+  it(" - send a string with more than maxLength and it should return false", () => {
+    let result = checkValidity("Discovery", {
+      required: true,
+      maxLength: 5
+    });
+    expect(result).toBeFalsy();
+  });
+
+  it(" - send a string with less than maxLength and it should return true", () => {
+    let result = checkValidity("Discovery", {
+      required: true,
+      maxLength: 10
     });
     expect(result).toBeTruthy();
   });
