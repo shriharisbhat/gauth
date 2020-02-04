957707c711b512eef09c9502106e8b66d68e182d
Raghavendra Mogeraya <raghavendra.mogeraya@robosoftin.com>
2020-02-04 13:23:34 +0530

    adding test cases for API call

--- src/components/Affiliates/affiliates.test.js
+++ src/components/Affiliates/affiliates.test.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import Affiliates from "./Affiliates";
+//jest.mock("axios");
+
+import axios from "axios";
+import nock from "nock";
+//import test from "ava"; // You can use any test framework.
+
 describe("Affiliates", () => {
   let wrapper;
   beforeEach(() => {
@@ -13,5 +19,34 @@ describe("Affiliates", () => {
 
   it("should initilize state values properly", () => {
     expect(wrapper.state()).toEqual({ affiliateList: [], error: null });
+  });
+
+  // test("should fetch affiliates", () => {
+  //   //const users = [{name: 'Bob'}];
+  //   const config = [];
+  //   const resp = { data: users };
+
+  //   //axios.get.mockResolvedValue(resp);
+
+  //   // or you could use the following depending on your use case:
+  //   axios.get.mockImplementation(() => Promise.resolve(resp));
+
+  //   return AffiliateService.getAffiliateList().then(data =>
+  //     expect(data).toEqual(users)
+  //   );
+  // });
+
+  axios.defaults.adapter = require("axios/lib/adapters/http");
+  jest.setTimeout(2000);
+  it("can fetch test response", async t => {
+    // Set up the mock request.
+    const scope = nock("http://demo4760291.mockable.io")
+      .get("/partners")
+      .reply(200, "{data:{}}");
+
+    await axios.get("http://demo4760291.mockable.io/partners");
+
+    // Assert that the expected request was made.
+    scope.done();
   });
 });
