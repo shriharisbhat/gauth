229cb9bbc92f46927934bd1af9bb65afdb4071bb
Raghavendra Mogeraya <raghavendra.mogeraya@robosoftin.com>
2020-02-04 16:25:35 +0530

    unit test added for shadow box

--- src/UI/ShadowBox/index.test.js
+++ src/UI/ShadowBox/index.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 import ShadowBox from "./index";
+import { create } from "react-test-renderer";
 describe("ShadowBox", () => {
   let wrapper;
   beforeEach(() => {
@@ -9,5 +10,13 @@ describe("ShadowBox", () => {
 
   it("should render ShadowBox component ", () => {
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should call props.onClick, when logo image is clicked", () => {
+    const fn = jest.fn();
+    let tree = create(<ShadowBox onClick={fn} />);
+    const img = tree.root.findByType("img");
+    img.props.onClick();
+    expect(fn.mock.calls.length).toBe(1);
   });
 });
